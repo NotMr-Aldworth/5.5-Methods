4ec7c7e81ba33bd33304b8feb4a70b55c4e41548
cowajack474 <cowajack474@2260I-L42-76>
2022-03-30 11:05:39 -0400

    gg

--- 5.5 Methods/Program.cs
+++ 5.5 Methods/Program.cs
@@ -10,7 +10,53 @@ namespace _5._5_Methods
     {
         static void Main(string[] args)
         {
-            //hello
+            int selection;
+            bool done = false;
+
+
+            Console.WriteLine("Enter a number for an animal");
+            Console.WriteLine("1 for a Camel");
+            Console.WriteLine("2 for a Bat");
+            Console.WriteLine("3 for a Philly Cheese Steak");
+
+            while (Int32.TryParse(Console.ReadLine(), out selection))
+            {
+                if (selection == 1)
+                    Camel();
+                else if (selection == 2)
+                    Bat();
+                else if (selection == 3)
+                    Philip();
+                else
+                    Console.WriteLine("Please enter a valid selection");
+
+            }
+            Console.ReadLine();
         }
+     public static void Camel()
+        {
+            Console.WriteLine("       __");
+            Console.WriteLine("      |  |");
+            Console.WriteLine("     (|00|)");
+            Console.WriteLine("      (==)");
+            Console.WriteLine("    ___||___");
+            Console.WriteLine("   / _ .. _ \\");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+     public static void Bat()
+        {
+            Console.WriteLine("Bat");
+        }
+    public static void Philip()
+        {
+            Console.WriteLine("Philly");
+        }
+    
     }
+
+
 }
